f3833c8c66cf3dfb27ca6b413198c376b615ffb1
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-02-15 18:51:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function loadIssue() {
+function loadIssues() {
    let template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
    let result = template(issues);
    document.getElementsByTagName("main")[0].innerHTML += result;
